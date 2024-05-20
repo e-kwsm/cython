922c61a2ababa94a103a767a1d7856c6a4e2a44b
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-05-20 14:55:42 +0900

    fix: fix invalid escape sequences

--- tests/run/r_mang1.pyx
+++ tests/run/r_mang1.pyx
@@ -11,4 +11,4 @@ __doc__ = u"""
 # this is a string constant test, not a test for 're'
 
 import re
-t = re.search(u'(\d+)', u'-2.80 98\n').groups()
+t = re.search(u'(\\d+)', u'-2.80 98\n').groups()
